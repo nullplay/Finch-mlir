8cf222df8ff4abba6eb0201509b90cd3124465eb
Mateusz Sokół <mat646@gmail.com>
2024-10-17 20:47:14 +0000

    Use standalone build mode

--- setup.py
+++ setup.py
@@ -23,36 +23,62 @@ class CMakeBuild(build_ext):
         extdir = ext_fullpath.parent.resolve()
         install_dir = extdir
 
-        cmake_args = [
+        llvm_cmake_args = [
             "-G Ninja",
-            f"-B{build_temp}",
+            f"-B{llvm_build_dir}",
             "-DLLVM_ENABLE_PROJECTS=mlir;llvm;clang;lld",
             "-DLLVM_TARGETS_TO_BUILD=Native",
             "-DCMAKE_BUILD_TYPE=Release",
-            f"-DCMAKE_INSTALL_PREFIX={install_dir}",
             "-DMLIR_ENABLE_BINDINGS_PYTHON=ON",
             f"-DPython3_EXECUTABLE={PYTHON_EXECUTABLE}",
             "-DLLVM_INSTALL_UTILS=ON",
-            f"-DMLIR_BINARY_DIR={build_temp}",
-            f"-DCMAKE_MODULE_PATH={LLVM_SOURCE_DIR}/mlir/cmake/modules",
-            "-DLLVM_EXTERNAL_PROJECTS=finch-dialect",
-            f"-DLLVM_EXTERNAL_FINCH_DIALECT_SOURCE_DIR={FINCH_MLIR_SOURCE_DIR}",
         ]
 
         subprocess.run(
-            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True,
+            ["cmake", ext.sourcedir, *llvm_cmake_args], cwd=llvm_build_dir, check=True,
+        )
+
+        subprocess.run(["ninja"], cwd=llvm_build_dir, check=True)
+
+        subprocess.run(
+            ["cmake", f"-DCMAKE_INSTALL_PREFIX={llvm_install_dir}", "-Pcmake_install.cmake"],
+            cwd=llvm_build_dir,
+            check=True,
+        )
+
+        dialect_cmake_args = [
+            "-G Ninja",
+            f"-B{finch_build_dir}",
+            f"-DMLIR_DIR={llvm_install_dir}/lib/cmake/mlir",
+            f"-DLLVM_EXTERNAL_LIT={llvm_build_dir}/bin/llvm-lit",
+        ]
+
+        subprocess.run(
+            ["cmake", FINCH_MLIR_SOURCE_DIR, *dialect_cmake_args], cwd=finch_build_dir, check=True,
         )
 
         subprocess.run(
-            ["cmake", "--build", ".", "--target", "install"],
-            cwd=build_temp,
+            ["ninja"], cwd=finch_build_dir, check=True,
+        )
+
+        subprocess.run(
+            ["cmake", f"-DCMAKE_INSTALL_PREFIX={install_dir}", "-Pcmake_install.cmake"],
+            cwd=finch_build_dir,
             check=True,
         )
 
 
-build_temp = Path.cwd() / "build" / "temp"
-if not build_temp.exists():
-    build_temp.mkdir(parents=True)
+def create_dir(name: str) -> Path:
+    path = Path.cwd() / "build" / name
+    if not path.exists():
+        path.mkdir(parents=True)
+    return path
+
+
+llvm_build_dir = create_dir("llvm-build")
+llvm_install_dir = create_dir("llvm-install")
+finch_build_dir = create_dir("finch-build")
+
 
 setup(
     name="mlir_finch",
